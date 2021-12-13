1fc782d0269f1fddc976ba6f381fb65e0f46695d
Marta <martmajew@gmail.com>
2021-12-13 15:45:50 +0000

    Bowling challenge - cleaned up

--- exercises/javascript/main/bowling/bowling.js
+++ exercises/javascript/main/bowling/bowling.js
@@ -3,33 +3,29 @@ export const bowl = (rolls) => {
     const frames = [];
   
     for (let i = 0; i < rolls.length; i += 2) {
-      // total += rolls[i];
-  
       if (rolls[i] === 10) {
         frames.push([rolls[i]]);
         i--;
       } else {
         frames.push([rolls[i], rolls[i + 1]]);
-        // i++
       }
     }
   
     for (let i = 0; i < frames.length; i++) {
       const currentFrame = frames[i];
-      console.log(currentFrame);
   
       let scoreForFrame;
   
       const isStrike = currentFrame.length === 1;
       // const scoreFromCurrentFrame = isStrike ? currentFrame[0] : currentFrame[0] + currentFrame[1]
       // const scoreFromNextFrame = isStrike ? nextFrame[0] + nextFrame[1] : nextFrame[0]
+      // condition ? true : false
   
       if (isStrike) {
         scoreForFrame = currentFrame[0];
   
         const nextFrame = frames[i + 1];
         scoreForFrame += nextFrame[0] + nextFrame[1];
-  
       } else {
         scoreForFrame = currentFrame[0] + currentFrame[1];
         const isSpare = scoreForFrame === 10;
@@ -39,8 +35,6 @@ export const bowl = (rolls) => {
           scoreForFrame += nextFrame[0];
         }
       }
-  
-      console.log(frames);
   
       total += scoreForFrame;
     }
