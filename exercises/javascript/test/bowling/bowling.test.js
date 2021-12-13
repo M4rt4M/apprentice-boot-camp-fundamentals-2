60e043742e6bac7ad235dce4276797d073593efe
Marta <martmajew@gmail.com>
2021-12-13 12:30:44 +0000

    Bowling challenge - sums up all regul;ar rolls

--- exercises/javascript/test/bowling/bowling.test.js
+++ exercises/javascript/test/bowling/bowling.test.js
@@ -5,4 +5,13 @@ describe("Bowling tests", () => {
     const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     expect(bowl(rolls)).toBe(0);
   });
+
+  it("should calculate the score for a regular game of bowling", () => {
+    const rolls1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    expect(bowl(rolls1)).toBe(20);
+
+    const rolls2 = [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1];
+    expect(bowl(rolls2)).toBe(22);
+
+  });
 });
